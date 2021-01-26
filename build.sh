7011978d7ec3ef139fc458fb98cdd01ecb2552fd
Cedric Roijakkers <croijakkers@ilionx.com>
2021-01-26 11:58:43 +0100

    Complete overhaul of the base box: now building from Centos ISO

--- build.sh
+++ build.sh
@@ -3,18 +3,17 @@
 set -e
 
 # Set version info
-BOX_VERSION_BASE=1.1.4
+export BOX_VERSION_BASE=1.2.0
+export CENTOS_BASE_ISO="CentOS-7-x86_64-Minimal-2009.iso"
+export CENTOS_BASE_ISO_SHA256="07b94e6b1a0b0260b94c83d6bb76b26bf7a310dc78d7a9c7432809fb9bc6194a"
 
 # Set versions requested of main components (These will be used in Packer and passed to Ansible downstream)
-export BOX_BASE="bento/centos-7"
-export BOX_BASE_VERSION=202012.21.0
 export ANSIBLE_VERSION=2.10.5
 export MINIKUBE_VERSION=1.17.0
 export DOCKER_VERSION=20.10.2
 export KUBECTL_VERSION=1.20.2
 export HELM_VERSION=3.5.0
 export KUBETAIL_VERSION=1.6.12
-export GUESTADDITIONS_VERSION=6.1.18
 
 # Set versions of supported tools, if they don't match, a warning will be shown on screen
 export VIRTUALBOX_VERSION="6.1.18r142142"
@@ -28,6 +27,9 @@ export VAGRANT_CLOUD_BOX_NAME="centos7-minikube"
 # ############################################################################################## #
 # Below this point there should be no need to edit anything, unless you know what you are doing! #
 # ############################################################################################## #
+
+# Generate the final version of the box, adding the date string of today
+export BOX_VERSION=${BOX_VERSION_BASE}-$(date +'%Y%m%d')
 
 echo "Testing if all required tools are installed, please wait..."
 
@@ -84,11 +86,10 @@ else
 fi
 
 # Export dynamic versioning info
-export BOX_VERSION=${BOX_VERSION_BASE}-$(date +'%Y%m%d')
 commit=$(git --no-pager log -n 1 --format="%H")
 export BOX_VERSION_DESCRIPTION="
 ## Description
-This box is based on the ${BOX_BASE} box version ${BOX_BASE_VERSION}. I try to keep the builds up to date with the latest version of this box.
+This base box is based on a clean CentOS 7 minimal install. I try to keep the builds up to date with the latest version of this box.
 When the box boots it contains a running minikube, ready to deploy kubenetes manifests, and kubectl is pre configured for the vagrant user.
 Helm is installed to allow the immediate deployment of charts.
 
